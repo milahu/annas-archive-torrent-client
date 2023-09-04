b54bb3f75edd0d693b88600ca39a9e62456797b9
Milan Hauth <milahu@gmail.com>
2023-09-04 14:46:42 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# https://setuptools.pypa.io/en/latest/userguide/quickstart.html
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='cas_torrent',
+    version='0.0.1',
+    packages=find_packages(
+        where='src',
+        #include=['mypackage*'],  # ['*'] by default
+        #exclude=['mypackage.tests'],  # empty by default
+    ),
+    install_requires=[
+        'libtorrent',
+    ],
+)
